3ddf87d643f48b77e34bbf604faaa4d89c949b5e
Lumagrever <lumagrever@hotmail.com>
2023-05-19 21:15:30 -0300

    Actualizacion

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to Luma.
+          Edit <code>src/App.js</code> and save to Lumagrever.
         </p>
         <a
           className="App-link"
