dd5ad3b9bcb41d35c602f8a01df71d029cfd0128
Lumagrever <lumagrever@hotmail.com>
2023-06-30 12:23:00 -0300

    responsive de productos individuales agregado

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,12 +13,13 @@ const Product = () => {
   useEffect(() => {
     setDetails(location.state.item);
   }, []);
+
   return (
-    <div>
-      <div className="max-w-screen-xl mx-auto my-10 flex gap-10">
-        <div className="w-2/5 relative">
+    <div className="max-w-screen-xl mx-auto my-10 px-4 md:px-6">
+      <div className="flex flex-col md:flex-row gap-10">
+        <div className="w-full md:w-2/5 relative">
           <img
-            className="w-full h-[550px] object-cover"
+            className="w-full h-auto md:h-[550px] object-cover"
             src={details.img}
             alt="productImg"
           />
@@ -30,9 +31,9 @@ const Product = () => {
             )}
           </div>
         </div>
-        <div className="w-3/5 flex flex-col justify-center gap-12">
+        <div className="w-full md:w-3/5 flex flex-col justify-center gap-6">
           <div>
-            <h2 className="text-4xl font-semibold">{details.title}</h2>
+            <h2 className="text-3xl md:text-4xl font-semibold">{details.title}</h2>
             <div className="flex items-center gap-4 mt-3">
               <p className="line-through font-base text-gray-500">
                 ${details.oldPrice}
@@ -83,7 +84,7 @@ const Product = () => {
             </button>
           </div>
           <p className="text-base text-gray-500">
-            Categoria:{" "}
+            Categoría:{" "}
             <span className="font-medium capitalize"> {details.category} </span>
           </p>
         </div>
